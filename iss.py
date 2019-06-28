ceb8c41a785f1997d03047a55625e1a2caf0e850
goranaviani <goranaviani@gmail.com>
2019-06-28 21:18:52 +0200

    Getting the names of the astronauts and their space crafts name.

--- iss.py
+++ iss.py
@@ -5,8 +5,12 @@ def iss_crew(issApiUrl):
     result = requests.get(issApiUrl + issCrewUrl)
     if result.status_code == 200:
         message = result.json()
-        print("Anja, there are " + str(message['number']) + " cosmonauts in space right now.")
-        
+        print("\nAnja, there are " + str(message['number']) + " cosmonauts in space right now.")
+        print("-------------------------------------------------- \nAnd ther are: \n")
+        for x in message['people']:
+            print("* {} is in space craft {}. \n" .format(x['name'], x['craft']))
+    elif result.status_code == 404:
+        print("The page is not reachable")
     else:
         print("Something has good wrong.")
 
