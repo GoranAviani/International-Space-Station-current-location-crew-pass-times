1e82b317327d08ffcb80551bcfb30e5a9e943dcc
goranaviani <goranaviani@gmail.com>
2019-06-28 21:48:31 +0200

    Refractoring the code and creating get_result_from_iss function that takes care of get calls

--- iss.py
+++ iss.py
@@ -2,41 +2,37 @@ import requests
 import datetime
 
 def get_result_from_iss(fullApiUrl):
-    pass
+    result = requests.get(fullApiUrl)
+    if result.status_code == 200:
+        message = result.json()
+        return message
+
+    elif result.status_code == 404:
+        print("The page is not reachable")
+    else:
+        print("Something has good wrong.")
+
+
 
 def iss_crew(issApiUrl):
     issCrewUrl = "astros.json"
-    result = requests.get(issApiUrl + issCrewUrl)
-    if result.status_code == 200:
-        message = result.json()
-        print("\nAnja, there are " + str(message['number']) + " cosmonauts in space right now.")
-        print("-------------------------------------------------- \nAnd ther are: \n")
-        for x in message['people']:
-            print("* {} is in space craft {}. \n" .format(x['name'], x['craft']))
-    elif result.status_code == 404:
-        print("The page is not reachable")
-    else:
-        print("Something has good wrong.")
+    message = get_result_from_iss(issApiUrl + issCrewUrl)
 
+    print("\nAnja, there are " + str(message['number']) + " cosmonauts in space right now.")
+    print("-------------------------------------------------- \nAnd ther are: \n")
+    for x in message['people']:
+        print("* {} is in space craft {}. \n" .format(x['name'], x['craft']))
 
 def iss_location(issApiUrl):
     issLocationUrl = "iss-now.json"
-    result = requests.get(issApiUrl + issLocationUrl)
-    if result.status_code == 200:
-        message = result.json()
-        dateAndTime = datetime.datetime.fromtimestamp(message["timestamp"])
-
-        issCoordinates = message['iss_position']
-
-        print("\nAnja, we have made a request for ISS location at {}." .format(dateAndTime))
+    message = get_result_from_iss(issApiUrl + issLocationUrl)
 
 
-        print("\nCurrent location coordinate of the space station are latitude: {}, longitude: {}."
-              .format(issCoordinates['latitude'], issCoordinates['longitude']))
 
-    elif result.status_code == 404:
-        print("The page is not reachable")
-    else:
-        print("Something has good wrong.")
+    dateAndTime = datetime.datetime.fromtimestamp(message["timestamp"])
+    issCoordinates = message['iss_position']
+    print("\nAnja, we have made a request for ISS location at {}." .format(dateAndTime))
+    print("\nCurrent location coordinate of the space station are latitude: {}, longitude: {}."
+          .format(issCoordinates['latitude'], issCoordinates['longitude']))
 
 
